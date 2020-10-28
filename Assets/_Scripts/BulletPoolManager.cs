78e3fe13c45a61e111d7296c826e5fbb56523532
Kristian <kristian.menes@gmail.com>
2020-10-28 12:26:56 -0400

    Started The TODOs
    Nothing really works yet. We'll see if I can get any of it to go.

---
+++ Assets/_Scripts/BulletPoolManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+// TODO: Bonus - make this class a Singleton!
+
+[System.Serializable]
+public class BulletPoolManager : MonoBehaviour
+{
+    public GameObject bullet;
+    public BulletController bulletCon;
+    //TODO: create a structure to contain a collection of bullets
+
+    struct BullColl
+    {
+        int BltAmnt;
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletCon.manager = this;
+        // TODO: add a series of bullets to the Bullet Pool
+        Queue<GameObject> BltQ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //TODO: modify this function to return a bullet from the Pool
+    public GameObject GetBullet()
+    {
+        if (BltQ.empty == true)
+        {
+            return "Empty";
+        }
+        else
+        {
+            BltQ.pop();
+        }
+        return bullet;
+    }
+
+    //TODO: modify this function to reset/return a bullet back to the Pool 
+    public void ResetBullet(GameObject bullet)
+    {
+        BlyQ.push(GameObject);
+    }
+}
